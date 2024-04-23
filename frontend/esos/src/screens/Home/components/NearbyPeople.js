ea056424718642e929bd8cb74a499938642b56cc
Resul Caliskan <resul.caliskan@venhancer.com>
2024-04-23 14:44:38 +0300

    feat: added geolocation functionality

---
+++ frontend/esos/src/screens/Home/components/NearbyPeople.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, Modal, TouchableOpacity, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
+
+const NearbyPeopleModal = ({ modalVisible, nearbyPeople, onClose }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalView}>
+        <View style={styles.modalHeader}>
+          <Text style={styles.modalHeaderText}>Yakınlarımın Durumu</Text>
+          <TouchableOpacity onPress={onClose}>
+            <Text style={styles.closeButton}>Kapat</Text>
+          </TouchableOpacity>
+        </View>
+        {nearbyPeople.map((person, index) => (
+          <View style={styles.personStatus} key={index}>
+            <Text style={styles.personName}>{person.name}</Text>
+            <Text
+              style={[
+                styles.personStatusText,
+                person.status === "Güvende"
+                  ? { color: "limegreen" }
+                  : person.status === "Güvende Değil"
+                  ? { color: "red" }
+                  : { color: "white" },
+              ]}
+            >
+              {person.status}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  modalHeaderText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: Colors.black,
+  },
+  closeButton: {
+    fontSize: 16,
+    color: Colors.blue,
+  },
+  personStatus: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  personName: {
+    marginRight: 10,
+  },
+  personStatusText: {
+    fontSize: 16,
+  },
+});
+
+export default NearbyPeopleModal;

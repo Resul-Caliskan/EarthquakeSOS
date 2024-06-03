6443d35cce2a0b7d11da8e65a19ed4c5ec89d633
Resul Caliskan <resulcaliskansau@gmail.com>
2024-06-03 10:52:59 +0300

    fix: mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,42 @@
-const http = require("http");
+// const http = require("http");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const fs = require("fs");
+// const path = require("path");
+// const config = require("./src/config/config");
+// const CoordinateRoutes = require("./src/routes/coordinateRoutes");
+// const AuthRoutes = require("./src/routes/authRoutes");
+// const socketConfig = require("./src/config/notificationConfig");
+// const notificationRoutes = require("./src/routes/notificationRoutes");
+
+// const app = express();
+// const dataDir = path.join(__dirname, "datas");
+// if (!fs.existsSync(dataDir)) {
+//   fs.mkdirSync(dataDir);
+// }
+// app.use("/datas", express.static(dataDir));
+
+// const server = http.createServer(app);
+// socketConfig.initializeSocket(server);
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
+// app.use("/api", CoordinateRoutes);
+// app.use("/api", AuthRoutes);
+// app.use("/api/notifications", notificationRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -6,6 +44,7 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const config = require("./src/config/config");
+const { connectToMongo, getBucket } = require("./src/config/mongo");
 const CoordinateRoutes = require("./src/routes/coordinateRoutes");
 const AuthRoutes = require("./src/routes/authRoutes");
 const socketConfig = require("./src/config/notificationConfig");
@@ -18,15 +57,17 @@ if (!fs.existsSync(dataDir)) {
 }
 app.use("/datas", express.static(dataDir));
 
-const server = http.createServer(app);
-socketConfig.initializeSocket(server);
+socketConfig.initializeSocket(app);
 
 app.use(bodyParser.json());
 app.use(cors());
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => {
+    console.log("Connected to MongoDB");
+    connectToMongo(); // Connect to MongoDB and initialize GridFS bucket
+  })
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api", CoordinateRoutes);
@@ -34,6 +75,6 @@ app.use("/api", AuthRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
